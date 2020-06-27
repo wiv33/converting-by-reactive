c127be7227b2c4014160c74f9be7eb90750f0dbd
ps <wiv821253@gmail.com>
2020-06-28 07:06:12 +0900

    move test code - from init to entity

--- src/test/java/org/psawesome/rsocketmongovue/RsocketMongoVueApplicationTests.java
+++ src/test/java/org/psawesome/rsocketmongovue/RsocketMongoVueApplicationTests.java
@@ -1,18 +1,14 @@
 package org.psawesome.rsocketmongovue;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.psawesome.rsocketmongovue.domain.user.entity.PsUser;
-import org.psawesome.rsocketmongovue.domain.user.entity.dto.PsUserDto;
-import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = {
@@ -52,34 +48,4 @@ class RsocketMongoVueApplicationTests {
     );
   }
 
-
-  @Test
-  @DisplayName("setUp 시 save 확인")
-  void testSetUpSaveMongo() {
-    PsUser user = PsUser.builder()
-            .name("ps")
-            .phone("010-0000-0000")
-            .email("psk@gmail.com")
-            .build();
-
-    Publisher<PsUser> save = reactiveMongoTemplate.save(user);
-    PsUserDto expected = PsUserDto.builder()
-            .name("ps")
-            .email("psk@gmail.com")
-            .phone("010-0000-0000")
-            .build();
-    StepVerifier.create(save).consumeNextWith(response ->
-            assertAll(
-                    () -> assertEquals(response, user),
-                    () -> assertEquals(expected.getName(), response.getName()),
-                    () -> assertEquals(expected.getPhone(), response.getPhone()),
-                    () -> assertEquals(expected.getEmail(), response.getEmail()),
-                    () -> assertThrows(NullPointerException.class, () -> System.out.println("failTest = " + "failTest"))
-            )
-    )
-            .expectComplete()
-            .verify()
-    ;
-
-  }
 }
---
+++ src/test/java/org/psawesome/rsocketmongovue/domain/user/entity/PsUserTest.java
@@ -0,0 +1,58 @@
+package org.psawesome.rsocketmongovue.domain.user.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.psawesome.rsocketmongovue.domain.user.entity.dto.PsUserDto;
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * author: ps [https://github.com/wiv33/rsocket-mongo-vue]
+ * DATE: 20. 6. 28. Sunday
+ */
+@SpringBootTest
+class PsUserTest {
+
+  @Autowired
+  ReactiveMongoTemplate reactiveMongoTemplate;
+
+  @Autowired
+  ReactiveMongoOperations operations;
+
+  @Test
+  @DisplayName("setUp 시 save 확인")
+  void testSetUpSaveMongo() {
+    PsUser user = PsUser.builder()
+            .name("ps")
+            .phone("010-0000-0000")
+            .email("psk@gmail.com")
+            .build();
+
+    Publisher<PsUser> save = reactiveMongoTemplate.save(user);
+    PsUserDto expected = PsUserDto.builder()
+            .name("ps")
+            .email("psk@gmail.com")
+            .phone("010-0000-0000")
+            .build();
+    StepVerifier.create(save).consumeNextWith(response ->
+            assertAll(
+//                    () -> assertThrows(NullPointerException.class, () -> System.out.println("failTest = " + "failTest")),
+                    () -> assertEquals(response, user),
+                    () -> assertEquals(expected.getName(), response.getName()),
+                    () -> assertEquals(expected.getPhone(), response.getPhone()),
+                    () -> assertEquals(expected.getEmail(), response.getEmail())
+            )
+    )
+            .expectComplete()
+            .verify()
+    ;
+
+  }
+}
