e8e430bd9f549022b69848209330ec38d09d9553
ps <wiv821253@gmail.com>
2020-07-03 22:16:06 +0900

    update entity

---
+++ src/main/java/org/psawesome/rsocketmongovue/domain/formatter/PsFormat.java
@@ -0,0 +1,12 @@
+package org.psawesome.rsocketmongovue.domain.formatter;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * author: ps [https://github.com/wiv33/rsocket-mongo-vue]
+ * DATE: 20. 7. 3. Friday
+ */
+@Document("PS_FORMAT")
+public class PsFormat {
+
+}
