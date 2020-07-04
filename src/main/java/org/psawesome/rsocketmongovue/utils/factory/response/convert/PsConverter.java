71ae914f83229b8d40dafdaab9ba80109813d117
ps <wiv821253@gmail.com>
2020-07-04 16:15:44 +0900

    add

---
+++ src/main/java/org/psawesome/rsocketmongovue/utils/factory/response/convert/PsConverter.java
@@ -0,0 +1,11 @@
+package org.psawesome.rsocketmongovue.utils.factory.response.convert;
+
+/**
+ * @author ps [https://github.com/wiv33/rsocket-mongo-vue]
+ * @since 20. 7. 4. Saturday
+ * @responsibility
+ * @role
+ * @cooperate
+ */
+public class PsConverter {
+}
