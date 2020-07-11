749c0dbf6dd7d31a3cd15a445f54267278cf9d7e
ps <wiv821253@gmail.com>
2020-07-11 17:19:14 +0900

    context test 수정

--- src/test/java/org/psawesome/rsocketmongovue/RsocketMongoVueApplicationTests.java
+++ src/test/java/org/psawesome/rsocketmongovue/RsocketMongoVueApplicationTests.java
@@ -1,11 +1,18 @@
 package org.psawesome.rsocketmongovue;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.util.MimeTypeUtils;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -17,6 +24,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 //@EnableKafka
 @EnableReactiveMongoRepositories
 class RsocketMongoVueApplicationTests {
+
+  private static RSocketRequester requester;
+
+  @BeforeAll
+  static void beforeAll(@Autowired RSocketRequester.Builder builder, @Value("${spring.rsocket.server.port}") int port) {
+    requester = builder.dataMimeType(MimeTypeUtils.APPLICATION_JSON)
+            .connectTcp("localhost", port)
+            .block(Duration.ofSeconds(3));
+  }
+
+  @AfterEach
+  void tearDown() {
+    requester.rsocket().dispose();
+  }
+
 
   @Autowired
   ReactiveMongoTemplate reactiveMongoTemplate;
