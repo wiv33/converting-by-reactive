1dbc2800b3bdb89ee74eafed8584e3f35502068b
ps <wiv821253@gmail.com>
2020-07-11 17:17:46 +0900

    파일 경로 변경

--- src/test/java/org/psawesome/rsocketmongovue/domain/doc/entity/PsArticleTest.java
+++ src/test/java/org/psawesome/rsocketmongovue/domain/doc/entity/PsArticleTest.java
@@ -1,14 +1,10 @@
 package org.psawesome.rsocketmongovue.domain.doc.entity;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,15 +12,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.AccessibleObject;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PsArticleTest {
 
@@ -68,17 +59,10 @@ class PsArticleTest {
 
     });
 
-
-
   }
 
   private void makeJson(ResponseEntity<String> forEntity) throws IOException {
-    var str = "C:\\private\\projects\\rsocket-mongo-vue\\src\\test\\java\\org\\psawesome\\rsocketmongovue\\domain\\doc\\entity\\json";
-    File file = new File(str);
-    if (!file.exists()) {
-      file.mkdirs();
-    }
-    File file1 = new File(file, "result.json");
+    File file1 = new File("doc_result.json");
     if (!file1.exists()) {
       file1.createNewFile();
     }
