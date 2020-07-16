1fd8fe3be05ffce3a65fcb933c4702d8318e1b37
ps <wiv821253@gmail.com>
2020-07-16 22:10:40 +0900

    eventSource switch

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -4,32 +4,46 @@
       <v-btn color="black" @click="openSse('qqq')">1</v-btn>
       <v-btn color="purple" @click="openSse('www')">2</v-btn>
       <v-btn color="pink" @click="openSse('eee')">3</v-btn>
+      <v-btn color="red" @click="eventSourceClose()">close</v-btn>
     </v-btn-toggle>
     <v-banner v-text="name">
     </v-banner>
+    <v-list-group>
+      <v-list-item v-for="(item, index) in transformedData" :key="index">
+        {{item}}
+      </v-list-item>
+    </v-list-group>
   </div>
 </template>
 
 <script>
+
   export default {
     data: () => {
       return {
         name: 'psawesome',
-        transformedData: []
+        transformedData: [],
+        es: undefined
       }
     },
     methods: {
       openSse(url) {
-        console.log(url);
-        let es = new EventSource(`http://psawesome.org:8080/api/v1/transformed/${url}`);
+        this.transformedData = [];
+        this.es = new EventSource(`http://psawesome.org:8080/api/v1/transformed/${url}`);
         this.name = `${"url is " + url}`;
-        es.addEventListener("message", (msg) => {
-          console.log(msg.data);
-          this.name = (msg.data);
+        this.es.addEventListener("message", (msg) => {
+          this.transformedData.push(msg.data);
         })
-        es.onerror = (err) =>  {
+        this.es.onerror = (err) => {
           console.log(err);
-          es.close();
+          this.es.close();
+        }
+      },
+      eventSourceClose() {
+        try {
+          this.es.close();
+        }catch (e) {
+          console.log(e);
         }
       }
     }
