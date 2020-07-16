ac7e08aa18b79d710c11ffd24b36c61cb90a37eb
ps <wiv821253@gmail.com>
2020-07-16 21:38:05 +0900

    how to testing eventsource

--- front/tests/unit/HelloVue.spec.js
+++ front/tests/unit/HelloVue.spec.js
@@ -14,7 +14,16 @@ describe('HelloWorldVue-test', function () {
     expect(wrapper.vm.$el.querySelectorAll("[type=button]").length).to.equal(3)
   });
 
-  it('init rendering visit test', function () {
-
+  it('init rendering visit test', done => {
+    const url = "qqq";
+    const eventSourceInitDict = {headers: {'Cookie': 'test=test'}};
+    const es = new EventSource(`http://psawesome.org:8080/api/v1/transformed/${url}`, eventSourceInitDict);
+    es.onmessage = (e) => {
+      expect(e.data).exist.equal("");
+      done()
+    }
+    es.onerror = (e) => {
+      console.log(e)
+    }
   });
 });
