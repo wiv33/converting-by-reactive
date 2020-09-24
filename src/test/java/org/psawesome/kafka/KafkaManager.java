a3a4af3f4266d123fa73261158d43aea6acefab5
ps <wiv821253@gmail.com>
2020-09-24 23:15:26 +0900

    update front-end

--- src/test/java/org/psawesome/kafka/KafkaManager.java
+++ src/test/java/org/psawesome/kafka/KafkaManager.java
@@ -15,9 +15,7 @@ import reactor.kafka.sender.SenderOptions;
 import reactor.kafka.sender.SenderRecord;
 import reactor.kafka.sender.SenderResult;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 /**
