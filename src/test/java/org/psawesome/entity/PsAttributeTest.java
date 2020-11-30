e1d6f184058d78e839f48d10c93d7b9f53d5720d
PSK <wiv821253@gmail.com>
2020-11-30 16:37:40 +0900

    add test code insert, select

--- src/test/java/org/psawesome/entity/PsAttributeTest.java
+++ src/test/java/org/psawesome/entity/PsAttributeTest.java
@@ -1,19 +1,46 @@
 package org.psawesome.entity;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
 
+import static org.springframework.data.relational.core.query.Criteria.where;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PsAttributeTest {
 
   @Autowired
   DatabaseClient databaseClient;
+
+  @AfterAll
+  void afterAll() {
+    final Mono<Integer> log = databaseClient.delete()
+            .from(PsAttribute.class)
+            .matching(where("attribute_key").is("ps_is__"))
+            .fetch()
+            .rowsUpdated()
+            .log();
+
+    StepVerifier.create(log)
+            .expectNext(3)
+            .verifyComplete()
+    ;
+  }
 
   @Test
   void testFindAll() {
@@ -29,4 +56,45 @@ class PsAttributeTest {
             .verify()
     ;
   }
+
+  @DisplayName("조회하기")
+  @ParameterizedTest
+  @ValueSource(strings = {"psawesome", "mybodyis", "numerics is"})
+  void testFindOne(String attribute_value) {
+    final Mono<Integer> insert = databaseClient.insert()
+            .into(PsAttribute.class)
+            .table("TTB_CMSC_TEMPLATE_ATTRIBUTE")
+            .using(PsAttribute.builder()
+                    .siteCd("JAI")
+                    .childSeq("7")
+                    .attributeKey("ps_is__")
+                    .attributeValue(attribute_value)
+                    .attributeRemark("")
+                    .modSeq("1925")
+                    .modDt(LocalDateTime.now())
+                    .regSeq("1925")
+                    .regDt(LocalDateTime.now())
+                    .build())
+            .fetch()
+            .rowsUpdated();
+
+    StepVerifier.create(insert)
+            .expectNextCount(1)
+            .verifyComplete()
+    ;
+
+
+    final Mono<PsAttribute> attr = databaseClient.select()
+            .from(PsAttribute.class)
+            .matching(where("attribute_value").is(attribute_value))
+            .fetch()
+            .first()
+            .log();
+
+    StepVerifier.create(attr)
+            .expectNextCount(1)
+            .verifyComplete();
+  }
+
+
 }
